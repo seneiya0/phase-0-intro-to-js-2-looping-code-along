9ef428c60b2a138fd4cc6db8216dad0b13478021
Seneiya Cooke <seneiyacooke@gmail.com>
2021-07-08 07:06:00 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names,) {
+  let message = [];
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return message;
+}
+
+writeCards(names); 
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+}
